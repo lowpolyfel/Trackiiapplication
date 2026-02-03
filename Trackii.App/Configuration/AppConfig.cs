295f1dfbb2c3da20e05bc65f2d32bfe14cd49cfa
hayfel <hayfeldev@gmail.com>
2026-02-03 08:49:26 -0700

    appconfig

--- Trackii.App/Configuration/AppConfig.cs
+++ Trackii.App/Configuration/AppConfig.cs
@@ -2,5 +2,5 @@ namespace Trackii.App.Configuration;
 
 public static class AppConfig
 {
-    public const string ApiBaseUrl = "http://192.168.100.7:8080/";
+    public const string ApiBaseUrl = "http://192.168.0.11:8081/";
 }
